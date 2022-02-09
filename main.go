0797a02752b61f6d552e9ed699b9c97a66d60f40
htkcodes <rkgetten@gmail.com>
2022-02-09 02:23:29 +0100

    .

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+type contactinfo struct {
+	email   string
+	zipcode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contactinfo
+}
+
+func main() {
+	jim := person{
+		"Jim",
+		"Party",
+		contactinfo{"lmeow", 30},
+	}
+	//jimPointer := &jim
+
+	jim.updateName("Roln")
+	jim.print()
+}
+
+func (p *person) updateName(newName string) {
+	(*p).firstName = newName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
